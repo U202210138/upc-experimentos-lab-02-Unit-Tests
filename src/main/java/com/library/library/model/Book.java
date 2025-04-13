e1bc34ae24485311cfa0bc19353ba17c0f0e0da7
U202210138 <u202210138@upc.edu.pe>
2025-04-12 23:07:01 -0500

    feat: added model Book

---
+++ src/main/java/com/library/library/model/Book.java
@@ -0,0 +1,48 @@
+package com.library.library.model;
+public class Book {
+
+    private String title;
+    private String author;
+
+    // Constructor
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    // Getters and Setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    // Override equals and hashCode to compare Books correctly
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Book book = (Book) obj;
+        return title.equals(book.title) && author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * title.hashCode() + author.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Book{title='" + title + "', author='" + author + "'}";
+    }
+}
